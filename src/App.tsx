e7ce3c3d2bebd7b3c710c9a29eb5df8d1dd0d6a9
Jiffy tech <jiffytechsolution@gmail.com>
2025-11-25 17:59:11 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-// import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+
 import Header from './Components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Pannel from './Components/Pannel/Pannel';
 import Documentation from './Components/Documentation/Documentation';
 import SearchResults from './Components/SearchResults/SearchResults';
-// import Blocks from './Components/Blocks/Blocks';
-// import Charts from './Components/Charts/Charts';
 import Themes from './Components/Themes/Themes';
 import Colors from './Components/Colors/Colors';
 import { ThemeProvider } from './contexts/ThemeContext';
@@ -26,8 +22,6 @@ function App() {
                 <Route path="/docs" element={<Documentation />} />
                 <Route path="/search" element={<SearchResults />} />
                 <Route path="/pannel/*" element={<Pannel />} />
-                {/* <Route path="/blocks" element={<Blocks />} /> */}
-                {/* <Route path="/charts" element={<Charts />} /> */}
                 <Route path="/themes" element={<Themes />} />
                 <Route path="/colors" element={<Colors />} />
               </Route>
